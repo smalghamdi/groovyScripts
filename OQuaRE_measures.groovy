ff4af53fd1d9148b18fc385225c8c4e273ce6b5c
smalghamdi <sarah.alghamdi.1@kaust.edu.sa>
2018-10-23 13:42:40 +0300

    Update OQuaRE_measures.groovy

--- OQuaRE_measures.groovy
+++ OQuaRE_measures.groovy
@@ -52,77 +52,96 @@ for (final OWLSubClassOfAxiom subClasse : ont.getAxioms(AxiomType.SUBCLASS_OF))
 
 //println (hasChild[fac.getOWLThing()].toString())
 numberOfLeafs = 0
-//numberOfCompositeLeafs = 0
+numberOfCompositeLeafs = 0
 leafs=[]
-//CompositeLeafs=[]
+CompositeLeafs=[]
 depthOfpath=[:]
-//depthOfCompositepath=[:]
+depthOfCompositepath=[:]
 
 ont.getClassesInSignature(true).each {cl ->
     if(!(hasChild[cl] instanceof List)){
         numberOfLeafs++
         leafs.add(cl)
- //       if(cl.toString().indexOf("phenomebrowser")>-1){
- //           numberOfCompositeLeafs++
- //           CompositeLeafs.add(cl)
- //       }
+        if(cl.toString().indexOf("phenomebrowser")>-1){
+            numberOfCompositeLeafs++
+            CompositeLeafs.add(cl)
+        }
     }
 
 }
+
+def recersiveDepth(node){
+    node = (hasParent[node] instanceof List)? hasParent[node]:null
+    if(node == null){
+        return 0
+    }else{
+        if(node.size()>1){
+            max = 0
+            maxnode = node[0]
+            node.each{n->
+                if(recersiveDepth(n)>max){
+                    max = recersiveDepth(n)
+                    maxnode = n
+                }
+            }
+            return (recersiveDepth(maxnode))+1
+        }else{
+            return recersiveDepth(node[0])+1
+        }
+    }
+}
+
 
 println("number of leafs = " + numberOfLeafs)
 sumDepth = 0
 leafs.each{ cl ->
     node = cl
-    depth = 0
-    while(node!=null){
-        node = (hasParent[node] instanceof List)? hasParent[node][0]:null
-        depth++
-        //println(node.toString() + "    = " +depth-1)
-
-    }
-    //println(cl.toString() + "    = " +(depth-1))
-    depthOfpath[cl]=(depth-1)
-    sumDepth+=(depth-1)
+    depth = recersiveDepth(node)
+    depthOfpath[cl]=(depth)
+    sumDepth+=(depth)
 }
 
-/*
 sumCompositeDepth = 0
 CompositeLeafs.each{ cl ->
     node = cl
-    depth = 0
-    while(node!=null){
-        node = (hasParent[node] instanceof List)? hasParent[node][0]:null
-        depth++
-        //println(node.toString() + "    = " +depth-1)
-
-    }
-    //println(cl.toString() + "    = " +(depth-1))
-    depthOfCompositepath[cl]=(depth-1)
-    sumCompositeDepth+=(depth-1)
+    depth = recersiveDepth(node)
+    depthOfCompositepath[cl]=(depth)
+    sumCompositeDepth+=(depth)
 }
-*/
+
 
 println("WMCOnto mean depth of paths = " + sumDepth/numberOfLeafs)
 println("DITOnto maximum depth = " + depthOfpath.max{it.value})
-/*
-println("WMCOnto (Composite) mean depth of paths = " + sumCompositeDepth/numberOfCompositeLeafs)
+
+if(numberOfCompositeLeafs>0){
+println("WMCOnto (Composite) mean depth of paths = " + sumCompositeDepth/numberOfCompositeLeafs)}
 println("DITOnto (Composite) maximum depth = " + depthOfCompositepath.max{it.value})
-*/
+
 numberOfClasses =0
-ont.getClassesInSignature(true).each {
+numberOfCompositeClasses=0
+ont.getClassesInSignature(true).each {cl ->
+    if(cl.toString().indexOf("phenomebrowser")>-1){
+        numberOfCompositeClasses++
+    }
     numberOfClasses++
 }
 
 ClassesWithMoreThanOneDirectedParent=0
+compositeClassesWithMoreThanOneDirectedParent=0
 numberOfParents.keySet().each{ cl ->
-    if(numberOfParents[cl] > 1){ClassesWithMoreThanOneDirectedParent++}
+    if(numberOfParents[cl] > 1){
+        ClassesWithMoreThanOneDirectedParent++
+        if(cl.toString().indexOf("phenomebrowser")>-1){
+            compositeClassesWithMoreThanOneDirectedParent++
+        }
+    }
     //println(cl.toString()+"\t"+numberOfParents[cl])
 }
 
 println("numberOfClasses = " +numberOfClasses)
 println("ClassesWithMoreThanOneDirectedParent = " +ClassesWithMoreThanOneDirectedParent)
 println("TMOnto Tangledness = " + (ClassesWithMoreThanOneDirectedParent/(numberOfClasses-1)))
-
-
+//println("TMOnto (Composite) Tangledness = " + (compositeClassesWithMoreThanOneDirectedParent/(numberOfCompositeClasses-1)))
+println("numberOfClasses = " +numberOfCompositeClasses)
+println("ClassesWithMoreThanOneDirectedParent = " +compositeClassesWithMoreThanOneDirectedParent)
 println("done")
